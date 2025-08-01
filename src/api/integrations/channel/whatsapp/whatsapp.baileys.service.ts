d1599a96fcc7a5d4242aaebfa57754f4c312d591
Stiwar0098 <brayan98macas@gmail.com>
2025-08-01 17:24:16 -0500

    fix: eliminar el código 405 de la lista de códigos para evitar reconexiones en BaileysStartupService

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -387,7 +387,7 @@ export class BaileysStartupService extends ChannelStartupService {
 
     if (connection === 'close') {
       const statusCode = (lastDisconnect?.error as Boom)?.output?.statusCode;
-      const codesToNotReconnect = [DisconnectReason.loggedOut, DisconnectReason.forbidden, 402, 406, 405];
+      const codesToNotReconnect = [DisconnectReason.loggedOut, DisconnectReason.forbidden, 402, 406];
       // const codesToNotReconnect = [DisconnectReason.loggedOut, 405];
       const shouldReconnect = !codesToNotReconnect.includes(statusCode);
       if (shouldReconnect) {
